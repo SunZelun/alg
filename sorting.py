7349998913482b48ef4ec0ead814e120836c8eb1
SG\jerry.sun <jerry.sun@bwoil.com>
2018-03-20 17:41:18 +0800

    update sorting

--- sorting.py
+++ sorting.py
@@ -2,6 +2,7 @@ import random
 import time
 
 
+# compare with next element and switch position if next element is greater, repeat until no more swaps
 def bubble_sort(array):
     swapped = True
     while swapped:
@@ -16,6 +17,7 @@ def bubble_sort(array):
     return array
 
 
+# consider first element as sorted array, compare next element with elements inside sorted array and place it before smaller numbers
 def insertion_sort(array):
     for i in range(1, len(array)):
         j = i - 1
@@ -30,6 +32,7 @@ def insertion_sort(array):
     return array
 
 
+# find smallest element and place it in front, repeat until no more element left
 def selection_sort(array):
     smallest = array[0]
     key = 0
@@ -49,6 +52,7 @@ def selection_sort(array):
     return array
 
 
+# break elements into arrays until only one element left inside each array, put smaller elements on the left and greater elements on the right
 def merge_sort(array):
     if len(array) <= 1:
         return array
@@ -82,12 +86,55 @@ def merge(left, right):
     return array
 
 
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        pivot = array[0]
+        left_array, right_array = [], []
+
+        for item in array[1:]:
+            if item <= pivot:
+                left_array.append(item)
+            else:
+                right_array.append(item)
+
+        return quick_sort(left_array) + [pivot] + quick_sort(right_array)
+
+
+def shell_sort(array):
+    gaps = [701, 301, 132, 57, 23, 10, 4, 1]
+
+    for gap in gaps:
+        i = gap
+        while i < len(array):
+            temp = array[i]
+            j = i
+
+            while j >= gap and array[j-gap] > temp:
+                array[j] = array[j-gap]
+                j -= gap
+            array[j] = temp
+            i += 1
+
+    return array
+
+
+def binary_search(array, element):
+    middle_element = array[len(array)/2] if len(array) >= 1 else None
+
+    while middle_element != element:
+        middle_element = array[len(array) / 2] if len(array) >= 1 else None
+        array = array[:len(array) / 2] if middle_element < element else array[len(array) / 2:]
+
+        return binary_search(array, element)
+
+    print "Found element on key: " + str(len(array)/2)
+
+
 numbers = []
 for x in range(1000):
-    num = random.randint(1, 100001)
-
-    while num in numbers:
-        num = random.randint(1, 101)
+    num = random.randint(1, 10000)
 
     numbers.append(num)
 
@@ -102,7 +149,7 @@ print "bubble spent: " + str(time.time()-now)
 i_number = list(numbers)
 now = time.time()
 print insertion_sort(i_number)
-print "insertion 1 spent: " + str(time.time()-now)
+print "insertion spent: " + str(time.time()-now)
 
 s_number = list(numbers)
 now = time.time()
@@ -113,3 +160,18 @@ m_number = list(numbers)
 now = time.time()
 print merge_sort(m_number)
 print "merge spent: " + str(time.time()-now)
+
+sp_number = list(numbers)
+now = time.time()
+print quick_sort(sp_number)
+print "quick spent: " + str(time.time()-now)
+
+sh_number = list(numbers)
+now = time.time()
+print shell_sort(sh_number)
+print "shell spent: " + str(time.time()-now)
+
+sh_number = list(numbers)
+now = time.time()
+print shell_sort(sh_number)
+print "binary search: " + str(time.time()-now)
